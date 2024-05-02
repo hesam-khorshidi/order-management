c92a57bcc58dbb533189069b7f0f46f4ff4712d0
Mohammad Hesam Khorshidi <m.hesam.khorshidi@gmail.com>
2024-05-02 16:36:41 +0330

    feat: implemented order repository and service feat: added hooks to check order before updating or inserting in database refactor: renamed Customer.go,Order.go and Provider.go files to lower case to match other files in project feat: implemented basic sms service

---
+++ controller/order_controller.go
@@ -0,0 +1,8 @@
+package controller
+
+type OrderController interface {
+	UpdateOrderStatus()
+}
+
+type orderController struct {
+}
---
+++ service/order_service.go
@@ -0,0 +1,50 @@
+package service
+
+import (
+	"fmt"
+	"order-management/entity"
+	"order-management/repository"
+)
+
+type OrderService interface {
+	UpdateOrderStatus(request entity.UpdateOrderRequest) error
+}
+
+type OrderServiceImpl struct {
+	repository repository.OrderRepositoryImpl
+	smsService SmsServiceImpl
+}
+
+func (service *OrderServiceImpl) UpdateOrderStatus(request entity.UpdateOrderRequest) error {
+	//checking if updated status is PICKED_UP
+	if request.Status == entity.PICKED_UP {
+		notified, err := service.repository.CheckNotifiedStatus(request.OrderReferenceId)
+		if err != nil {
+			return err
+		}
+		//If user is notified before then we skip the notification step
+		if notified {
+			return service.repository.UpdateOrderStatus(request)
+		}
+		//if not we notify them and update the record
+		receiverNumber, err := service.repository.GetOrderReceiverNumber(request.OrderReferenceId)
+		if err != nil {
+			return err
+		}
+		service.smsService.SendSms(fmt.Sprintf("Order number %d status changed to %s", request.OrderReferenceId, request.Status), receiverNumber)
+		return service.repository.UpdateOrderStatus(request)
+	} else {
+		//Get customer phone number from database
+		receiverNumber, err := service.repository.GetOrderReceiverNumber(request.OrderReferenceId)
+		if err != nil {
+			return err
+		}
+		//Notify User and update record
+		service.smsService.SendSms(fmt.Sprintf("Order number %d status changed to %s", request.OrderReferenceId, request.Status), receiverNumber)
+		return service.repository.UpdateOrderStatus(request)
+	}
+}
+
+func NewOrderService(r repository.OrderRepositoryImpl, smsService SmsServiceImpl) OrderService {
+	return &OrderServiceImpl{repository: r, smsService: smsService}
+}
